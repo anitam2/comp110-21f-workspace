8e1ddc43a50637230c7072b8bfacd3c75fc4693a
Anita Murali <amurali@unc.edu>
2021-10-18 21:06:32 -0400

    new

--- sandbox/dict.py
+++ sandbox/dict.py
@@ -1,6 +1,6 @@
 counts: dict[str, int] = {}
 
-example_string: str = "The quick brown fox jumped over the lazy dog. Then, Kyle petted the dog and called him Fluffernutter."
+example_string: str = "The quick brown fox jumped over the lazy dog."
 
 for letter in example_string.lower():
     if letter in counts:
